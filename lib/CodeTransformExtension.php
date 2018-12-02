d4af47d5107a822beb37203f06a293e0ec3214d6
Daniel Leech <dan.t.leech@gmail.com>
2018-12-02 10:19:16 +0000

    made public constants

--- lib/CodeTransformExtension.php
+++ lib/CodeTransformExtension.php
@@ -18,17 +18,19 @@ use RuntimeException;
 
 class CodeTransformExtension implements Extension
 {
+    const SERVICE_CODE_TRANSFORM = 'code_transform.transform';
+
     const TAG_FROM_EXISTING_GENERATOR = 'code_transform.from_existing_generator';
     const TAG_TRANSFORMER = 'code_transform.transformer';
     const TAG_NEW_CLASS_GENERATOR = 'code_transform.new_class_generator';
+    const SERVICE_CLASS_GENERATORS = 'code_transform.new_class_generators';
+    const SERVICE_CLASS_INFLECTORS = 'code_transform.from_existing_generators';
 
     /**
      * {@inheritDoc}
      */
     public function configure(Resolver $schema)
     {
-        $schema->setDefaults([
-        ]);
     }
 
     public function load(ContainerBuilder $container)
@@ -40,6 +42,10 @@ class CodeTransformExtension implements Extension
 
     private function registerTransformers(ContainerBuilder $container)
     {
+        $container->register(self::SERVICE_CODE_TRANSFORM, function (Container $container) {
+            return CodeTransform::fromTransformers($container->get('code_transform.transformers'));
+        });
+
         $container->register('code_transform.transformers', function (Container $container) {
             $transformers = [];
             foreach ($container->getServiceIdsForTag(self::TAG_TRANSFORMER) as $serviceId => $attrs) {
@@ -48,15 +54,11 @@ class CodeTransformExtension implements Extension
 
             return Transformers::fromArray($transformers);
         });
-
-        $container->register('code_transform.transform', function (Container $container) {
-            return CodeTransform::fromTransformers($container->get('code_transform.transformers'));
-        });
     }
 
     private function registerGenerators(ContainerBuilder $container)
     {
-        $container->register('code_transform.new_class_generators', function (Container $container) {
+        $container->register(self::SERVICE_CLASS_GENERATORS, function (Container $container) {
             $generators = [];
             foreach ($container->getServiceIdsForTag(self::TAG_NEW_CLASS_GENERATOR) as $serviceId => $attrs) {
                 $this->assertNameAttribute($attrs, $serviceId);
@@ -66,7 +68,7 @@ class CodeTransformExtension implements Extension
             return Generators::fromArray($generators);
         });
 
-        $container->register('code_transform.from_existing_generators', function (Container $container) {
+        $container->register(self::SERVICE_CLASS_INFLECTORS, function (Container $container) {
             $generators = [];
             foreach ($container->getServiceIdsForTag(self::TAG_FROM_EXISTING_GENERATOR) as $serviceId => $attrs) {
                 $this->assertNameAttribute($attrs, $serviceId);
@@ -81,14 +83,14 @@ class CodeTransformExtension implements Extension
     {
         $container->register('code_transform.rpc.handler.class_inflect', function (Container $container) {
             return new ClassInflectHandler(
-                $container->get('code_transform.from_existing_generators'),
+                $container->get(self::SERVICE_CLASS_INFLECTORS),
                 $container->get(ClassToFileExtension::SERVICE_CONVERTER)
             );
         }, [ RpcExtension::TAG_RPC_HANDLER => ['name' => ClassInflectHandler::NAME] ]);
 
         $container->register('code_transform.rpc.handler.class_new', function (Container $container) {
             return new ClassNewHandler(
-                $container->get('code_transform.from_existing_generators'),
+                $container->get(self::SERVICE_CLASS_INFLECTORS),
                 $container->get(ClassToFileExtension::SERVICE_CONVERTER)
             );
         }, [ RpcExtension::TAG_RPC_HANDLER => ['name' => ClassNewHandler::NAME] ]);
@@ -96,7 +98,7 @@ class CodeTransformExtension implements Extension
 
         $container->register('code_transform.rpc.handler.transform', function (Container $container) {
             return new TransformHandler(
-                $container->get('code_transform.transform')
+                $container->get(self::SERVICE_CODE_TRANSFORM)
             );
         }, [ RpcExtension::TAG_RPC_HANDLER => ['name' => TransformHandler::NAME] ]);
     }
