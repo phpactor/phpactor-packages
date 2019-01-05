38b9744c105ff826ab33d216e61d88e54dc8f18a
Baptiste Clavié <clavie.b@gmail.com>
2019-01-06 00:54:06 +0100

    Add previous exception into CouldNotInstallExtension

--- lib/Model/Exception/CouldNotInstallExtension.php
+++ lib/Model/Exception/CouldNotInstallExtension.php
@@ -2,8 +2,13 @@
 
 namespace Phpactor\Extension\ExtensionManager\Model\Exception;
 
+use Throwable;
 use RuntimeException;
 
 class CouldNotInstallExtension extends RuntimeException
 {
+    public function __construct(string $message, Throwable $previous = null)
+    {
+        parent::__construct($message, 0, $previous);
+    }
 }
