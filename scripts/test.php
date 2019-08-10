d482eb6d54e412051ee53a2ea4437ff0a21ed21e
Daniel Leech <dan.t.leech@gmail.com>
2019-08-10 22:40:33 +0100

    Use correct CWD

--- scripts/test.php
+++ scripts/test.php
@@ -16,7 +16,7 @@ $processes = [];
 $exitSum = 0;
 $fails = [];
 
-foreach ($packageMetas as $packageMeta) {
+foreach ($packageMetas as $packageName => $packageMeta) {
     echo sprintf('Enqueuing travis scripts for "%s"', $packageMeta['name']).PHP_EOL;
 
     $travisPath = $packageMeta['path'] . '/.travis.yml';
@@ -29,16 +29,17 @@ foreach ($packageMetas as $packageMeta) {
     $travisConfig = Yaml::parseFile($travisPath);
 
     foreach ($travisConfig['before_script'] ?? [] as $script) {
-        $queues = enqueue($queues, $packageMeta['name'], $script);
+        $queues = enqueue($queues, $packageName, $script);
     }
 
     foreach ($travisConfig['script'] ?? [] as $script) {
-        $queues = enqueue($queues, $packageMeta['name'], $script);
+        $queues = enqueue($queues, $packageName, $script);
     }
 }
 
 while ($queues) {
-    foreach ($queues as $packageName => &$scripts) {
+    foreach ($queues as $packageName => $scripts) {
+        $packageMeta = $packageMetas[$packageName];
         usleep(5000);
 
         if (empty($scripts)) {
@@ -48,7 +49,7 @@ while ($queues) {
         }
 
         if (!isset($processes[$packageName])) {
-            $processes = start_process($processes, $scripts, $packageName, $packageMeta);
+            [$processes,$scripts] = start_process($processes, $scripts, $packageName, $packageMeta);
             continue;
         }
 
@@ -63,43 +64,52 @@ while ($queues) {
         $process->stop();
 
         // process stopped
-        fwrite(STDOUT, sprintf('// [%s] %s EXITED %s', $packageName, $script, $process->getExitCode()).PHP_EOL);
+        fwrite(STDOUT, sprintf(
+            '// [%s] %s EXITED %s',
+            $packageName,
+            $process->getCommandLine(),
+            $process->getExitCode()
+        ).PHP_EOL);
         $exitSum += $process->getExitCode();
 
         if ($process->getExitCode() !== 0) {
             unset($queues[$packageName]);
-            $fails[] = [ $packageName, $process->getCommandLine()];
+            $fails[] = [ $packageName, $process->getCommandLine(), $process->getOutput(), $process->getErrorOutput()];
         }
 
         unset($processes[$packageName]);
         unset($process);
 
         // start a new one
-        $processes = start_process($processes, $scripts, $packageName, $packageMeta);
+        [ $processes, $scripts ] = start_process($processes, $scripts, $packageName, $packageMeta);
+        $queues[$packageName] = $scripts;
     }
 }
 
-foreach ($fails as [ $packageName, $script ]) {
-    fwrite(STDOUT, sprintf('Failed: %s / %s', $packageName, $script).PHP_EOL);
+foreach ($fails as [ $packageName, $script, $stdOut, $stdErr ]) {
+    fwrite(STDOUT, $line = sprintf('Failed: %s / %s', $packageName, $script).PHP_EOL);
+    fwrite(STDOUT, str_repeat('=', strlen($line)).PHP_EOL.PHP_EOL);
+    fwrite(STDOUT, $stdOut);
+    fwrite(STDOUT, $stdErr);
 }
 
 exit($exitSum);
 
-function start_process(array $processes, array &$scripts, string $packageName, array $packageMeta)
+function start_process(array $processes, array $scripts, string $packageName, array $packageMeta)
 {
-    if (count($processes) === 5) {
-        return $processes;
+    if (count($processes) === 10) {
+        return [$processes, $scripts];
     }
 
     $script = array_shift($scripts);
-    fwrite(STDOUT, sprintf('// [%s] %s', $packageName, $script).PHP_EOL);
-    $process = new Process($script, getcwd() . '/' . $packageMeta['path']);
+    $workingDir = getcwd() . '/' . $packageMeta['path'];
+    fwrite(STDOUT, sprintf('// [%s] [%s] %s', $packageName, $workingDir, $script).PHP_EOL);
+    $process = new Process($script, $workingDir);
     $process->start(function ($type, $data) {
-        echo $data;
     });
     $processes[$packageName] = $process;
 
-    return $processes;
+    return [$processes, $scripts];
 }
 
 function enqueue(array $queues, string $name, string $script): array
