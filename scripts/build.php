4a5e6e3fd3490cd5c6a1db85d78ee05d99e38bf0
Daniel Leech <dan.t.leech@gmail.com>
2019-11-17 11:29:46 +0000

    Removed old build scripts

--- scripts/build.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!file_exists(__DIR__ . '/../build')) {
-    mkdir(__DIR__ . '/../build', 0777);
-}
-echo `php scripts/generate-package-meta.php > build/package-meta.json`;
-echo `php scripts/generate-composer-json.php > composer.json.new`;
-echo `mv composer.json.new composer.json`;
-echo `php scripts/generate-split-list.php > build/split-list.php`;
